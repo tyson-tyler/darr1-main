be057aed33d31753d59c12dad84eee4b0b33e6d2
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2025-08-29 11:37:53 +0530

    Update footer.tsx

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -27,22 +27,22 @@ const Footer = () => {
 
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <Link href="#" className="hover:underline me-4 md:me-6">
+              <Link href="/comman/about" className="hover:underline me-4 md:me-6">
                 About
               </Link>
             </li>
             <li>
               <Link href="#" className="hover:underline me-4 md:me-6">
-                Privacy Policy
+                Product
               </Link>
             </li>
             <li>
               <Link href="#" className="hover:underline me-4 md:me-6">
-                Licensing
+                
               </Link>
             </li>
             <li>
-              <Link href="#" className="hover:underline">
+              <Link href="/comman/contact" className="hover:underline">
                 Contact
               </Link>
             </li>
