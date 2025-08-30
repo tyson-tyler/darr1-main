0e97dca459102bd7e8eac37e8da054ed2dbaf060
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2025-08-30 14:00:43 +0530

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,16 +1,50 @@
-import { ReactNode } from "react";
+"use client";
 
-interface AdminLayoutProps {
+import { AuthContextProvider, useAuth } from "@/context/authcontext";
+import AdminLayout from "./components/AuthLayout";
+import { useRouter } from "next/navigation";
+import { useEffect, ReactNode } from "react";
+import { FaCircleUp } from "react-icons/fa6";
+
+// Annotate props type
+type LayoutProps = {
   children: ReactNode;
-}
-
-const AdminLayout = ({ children }: AdminLayoutProps) => {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      {/* Navbar or Sidebar can go here */}
-      <main className="flex-1">{children}</main>
-    </div>
-  );
 };
 
-export default AdminLayout;
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <AuthContextProvider>
+ <AdminChecking>{children}</AdminChecking>    
+    
+   </AuthContextProvider>
+  );
+};
+export default Layout;
+
+function AdminChecking({ children }: LayoutProps) {
+  const { user, isLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user && !isLoading) {
+      router.push("/auth/login");
+    }
+  }, [user, isLoading, router]);
+
+  if (isLoading) {
+    return (
+      <div className="h-screen w-full flex justify-center items-center">
+        <FaCircleUp className="w-12 h-12 animate-spin" />
+      </div>
+    );
+  }
+  if (!user) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        Please Login First
+      </div>
+    );
+  }
+
+  return <AdminLayout>{children}</AdminLayout>;
+}
