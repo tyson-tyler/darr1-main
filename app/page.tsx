719af3846ef31ffbb70c6f35d138c0dac54e9c1f
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2025-08-31 15:58:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,26 @@
+export const revalidate = 0; // 👈 Always fetch fresh data from Firebase
+
 import {
   getFeaturedProducts,
   getProducts,
 } from "@/lib/firebase/products/read_server";
 import Collections from "./components/home/collections";
-
 import { getCollections } from "@/lib/firebase/collections/read_server";
 import { getCategories } from "@/lib/firebase/catergories/read_server";
-
 import CustomerReviews from "./components/home/customerreview";
 import Footer from "./components/footer/footer";
-
 import Loader from "./components/main/newarr/slider/MainSlider";
-
 import WeddingBanner from "./components/main/newarr/slider/wbanner";
 import ProductsGridView, { ProductCard } from "./components/home/Product";
-
 import HeroTitle1 from "./components/navbar/heading1";
 import HeroTitle2 from "./components/navbar/heading2";
-
 import ContactForm from "./comman/contact/page";
 import MainHeader from "./admin/components/MainHeader";
 import Mainnav from "./components/navbar/mainnav";
 import BlogSection from "./comman/blog/page";
+import HeroTitle from "./components/navbar/heading";
+import Categories from "./components/home/category";
+import CTAImage from "./components/cata";
 
 export default async function Home() {
   const [featuredProducts, collections, categories, products] =
@@ -34,40 +33,32 @@ export default async function Home() {
 
   return (
     <>
-    
+      <Loader />
       <Mainnav />
       <MainHeader />
-
       <div className="flex flex-col w-full justify-center items-center text-black bg-white">
-{/*         <HeroTitle /> 
-         <Categories categories={categories} />  */}
+        {/* <HeroTitle />
+        <Categories categories={categories} /> */}
       </div>
       <div className="bg-white w-full ">
         <HeroTitle1 />
         <Collections collections={collections} />
       </div>
-
-      {/* <div className="z-50 bg-black h-screen w-full">
-        <Minimal />
-      </div> */}
-
-{/*       <div className="px-5">
-        <WeddingBanner />
-      </div> */}
-
       <div className=" w-full mb-8">
         <HeroTitle2 />
         <ProductsGridView products={products} />
       </div>
+      <main className="px-4 md:px-8 py-8">
+        <CTAImage
+          href="/categories/1aIpbpPn4qWIDAm59MWk"
+          src="https://res.cloudinary.com/dblf5n0yn/image/upload/v1756630975/poiz2vba5bxwuhj9qxnp.jpg"
+          alt="Shop the Summer Sale"
+          priority
+        />
+      </main>
       <div className=" w-full mb-8">
-        <BlogSection/>
+        <BlogSection />
       </div>
-      
-
-      <div className="bg-white w-full ">
-        <CustomerReviews />
-      </div>
-
       <div>
         <ContactForm />
       </div>
