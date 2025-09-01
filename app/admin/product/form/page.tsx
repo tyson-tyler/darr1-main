a7bd44df82457feea1763f528a2b8a39b23e9e80
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2025-09-01 17:17:17 +0530

    Update page.tsx

--- app/admin/product/form/page.tsx
+++ app/admin/product/form/page.tsx
@@ -53,8 +53,7 @@ const Page = () => {
     try {
       await createNewProduct({
         data: data,
-     featureImage: featureImage || undefined, // optional
-      imageList: imageList.length > 0 ? imageList : undefined, 
+data: data, featureImage: featureImage, imageList: imageList,
       });
       setData(null);
       setFeatureImage(null);
